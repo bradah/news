1f4b37d1c5d0cb4412554d885e50d7fad6987835
Oleg Pascal <rtrn.0@ya.ru>
2021-05-20 18:33:49 +0300

    Experimenting

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,50 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Data
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.Time
+import           GHC.Generics
+
+import           Network.HTTP.Types
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+
 main :: IO ()
-main = print "not yet implemented"
+main = do
+    run 8000 app
+
+data API
+    = Root
+    | News
+
+routine :: Request -> API
+routine req = undefined
+
+app :: Application
+app req respond = do
+    conn <- connect connInfo
+    respond $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        "Hello, web!"
+  where
+    connInfo = defaultConnectInfo
+        { connectUser = "lad"
+        , connectPassword = "oi"
+        , connectDatabase = "test"
+        }
+
+data Person = Person
+    { id               :: Int
+    , first_name       :: String
+    , last_name        :: String
+    , email            :: Maybe String
+    , gender           :: String
+    , date_of_birth    :: Date
+    , country_of_birth :: String
+    } deriving (Show, Generic)
+
+instance FromRow Person where
--- src/Data.hs
+++ src/Data.hs
@@ -1,2 +1,70 @@
 module Data where
 
+import           Data.Text
+import           Data.Time.Calendar
+
+data User = User
+    { user'id            :: Int
+    , user'name          :: Text
+    , user'surname       :: Text
+    , user'photo         :: Photo
+    , user'login         :: Text
+    , user'password      :: Text
+    , user'creation_date :: Day
+    , user'is_admin      :: Bool
+    } deriving (Show)
+
+data Photo = Photo
+    { photo'id   :: Int
+    , photo'link :: Text
+    } deriving (Show)
+
+data Author = Author
+    { author'user_id :: Int
+    , author'bio     :: Text
+    } deriving (Show)
+
+data Category = Category
+    { category'id        :: Int
+    , category'name      :: Text
+    , category'parent_id :: Maybe Int
+    } deriving (Show)
+
+data Tag = Tag
+    { tag'id   :: Int
+    , tag'name :: Text
+    } deriving (Show)
+
+data Article = Article
+    { article'id               :: Int
+    , article'title            :: Text
+    , article'creation_date    :: Day
+    , article'author           :: Author
+    , article'category_id      :: Int
+    , article'tags             :: [Tag]
+    , article'text             :: Text
+    , article'main_photo       :: Photo
+    , article'secondary_photos :: [Photo]
+    , article'comments         :: [Comment]
+    , article'templates        :: [Template]
+    } deriving (Show)
+
+data Comment = Comment
+    { comment'id         :: Int
+    , comment'user_id    :: Int
+    , comment'article_id :: Int
+    , comment'text       :: Text
+    } deriving (Show)
+
+-- type Template = Article
+data Template = Template
+    { template'title            :: Text
+    , template'creation_date    :: Day
+    , template'author           :: Author
+    , template'category         :: Category
+    , template'tags             :: [Tag]
+    , template'text             :: Text
+    , template'main_photo       :: Photo
+    , template'secondary_photos :: [Photo]
+    , template'comments         :: [Comment]
+    } deriving (Show)
